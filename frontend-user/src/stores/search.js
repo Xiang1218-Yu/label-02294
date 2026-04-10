6a0bd925186588bddee7d3910b128730f9edcca9
Balala <1061366940@qq.com>
2026-04-10 13:25:18 +0800

    feature: 迭代

--- frontend-user/src/stores/search.js
+++ frontend-user/src/stores/search.js
@@ -2,14 +2,23 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useSearchStore = defineStore('search', () => {
+  // 搜索关键词
   const keyword = ref('')
+  // 是否处于搜索状态
   const isSearching = ref(false)
 
+  /**
+   * 设置搜索关键词
+   * @param {string} value - 搜索关键词
+   */
   function setKeyword(value) {
     keyword.value = value
     isSearching.value = !!value.trim()
   }
 
+  /**
+   * 清除搜索状态
+   */
   function clearSearch() {
     keyword.value = ''
     isSearching.value = false
